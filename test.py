70ec5ea0666de1e4284e3b6a6930270c12101214
Faroja <86199530+Faroja@users.noreply.github.com>
2025-02-03 15:22:38 +0700

    update kedua testing

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ list_buku = ["Laskar Pelangi", "Ayat-Ayat Cinta", "Genius Kampus"]
 
 for index, buku in enumerate(list_buku):
     print(index, buku)
+add_new_variabel = "apala"
+nambah_lagi = 'faroja'
