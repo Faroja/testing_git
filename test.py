7271a089a72457f4fdc2d7ff2f924078254b4478
Muhamad Faroja <86199530+Faroja@users.noreply.github.com>
2025-02-04 15:04:38 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-
-def good_morning(name):
-    global greeting
-    greeting = 'Good Night'
-    print(f"{greeting}, {name}!") 
-greeting = 'Good Morning'
-#output apa?
-good_morning("Alice")
+conflict_test = print('satu dua tiga')
